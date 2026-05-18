61f387dfd14a108ba4796b380088da652db1e32b
gtkrshnaaa <114217089+gtkrshnaaa@users.noreply.github.com>
2026-05-19 03:58:22 +0800

    db: remove redundant schema.sql to prevent duplication

--- database/schema/schema.sql
+++
@@ -1,7 +0,0 @@
--- Database schema definition for GOVEMVC Todo application
-
-CREATE TABLE IF NOT EXISTS todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    completed INTEGER NOT NULL DEFAULT 0
-);
